1117c67e4074c717077e6ca1f87a59a15375935a
MicroFARADHENRY <chanchito1105@gmail.com>
2021-07-01 14:43:37 -0700

    INITIAL COMMIT

---
+++ cash.py
@@ -0,0 +1,12 @@
+print("Hello World")
+print("Please enter a product price to begin")
+
+
+p_r1=input("Enter Product price:")
+p_r2=input("Enter Product price:")
+p_r3=input("Enter Product price:")
+p_r4=input("Enter Product price:")
+result=float(p_r1)+float(p_r2)+float(p_r3)+float(p_r4)
+print("Total Equals:")
+print(result)
+print("thank you for choosing us")
